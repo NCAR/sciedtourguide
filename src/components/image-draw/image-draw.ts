dba6741e1036a830f298d9cf3bfeeb4412b15710
Sharon Clark <clarksl@gmail.com>
2017-10-26 09:41:13 -0600

    positioning of postcard components works

--- src/components/image-draw/image-draw.ts
+++ src/components/image-draw/image-draw.ts
@@ -12,15 +12,17 @@ export class ImageDrawTextDirective {
     loaded = false;
     message = '';
     @Input() text: string;
-    @Input() color: string;
     @Input() bkg: string;
-    @Input() orientation: string;
     @HostListener('load', ['$event.target'])
 
   onLoad(img) {
     if(this.loaded) {
       return;
     }
+
+
+    let scaled_width;
+    let scaled_height;
 
     function getLines(ctx, text, maxWidth) {
         var words = text.split(" ");
@@ -47,51 +49,63 @@ export class ImageDrawTextDirective {
 
     let canvas = document.createElement('canvas');
     let context = canvas.getContext('2d');
+    // set size of postcard canvas
+    canvas.height = 1200;
+    canvas.width = 1600;
+
+    // draw linear gradient background-color
+    console.log('draw background gradient');
+    var my_gradient=context.createLinearGradient(0,0,canvas.width/2,0);
+    my_gradient.addColorStop(0,"#003565");
+    my_gradient.addColorStop(0.5,"#1C497A");
+    my_gradient.addColorStop(1,"#000000");
+    context.fillStyle=my_gradient;
+    context.fillRect(0,0,(canvas.width/2)+50, canvas.height);
+
+
+
+
 
     //load bkg img
     let bkgObject = document.createElement("img");
     bkgObject.onload = (data) => {
-    // set canvas the size of the bkgobject
-      canvas.height = bkgObject.height;
-      canvas.width = bkgObject.width;
+        // draw library image
+        console.log('draw lib image');
+        context.drawImage(bkgObject, (canvas.width/2)+50, 0, canvas.width - ((canvas.width/2)+50), canvas.height);
 
-      // draw bkg img
-      context.drawImage(bkgObject, 0, 0, canvas.width, canvas.height);
-
-      if(this.orientation == 'portrait'){
         // draw user image
+
+        console.log('draw photo');
         // get scaled down width and height for user image
-        let scaled_width = (canvas.width/2)-50;
-        let scaled_height = ((scaled_width * img.height)/img.width)-50;
-        context.drawImage(img, 100, 100, scaled_width, scaled_height);
+        scaled_width = (canvas.width/2)-50;
+        scaled_height = ((scaled_width * img.height)/img.width)-50;
+        context.drawImage(img, 50, ((canvas.height/2)-(scaled_height/2)), scaled_width, scaled_height);
+
+
+
 
         // draw transparent text bkg
+        console.log('draw text box');
         context.fillStyle = 'rgba(225,225,225,0.5)';
-        let textbox = {'x': scaled_width+200, 'y': (scaled_height/2)+100, 'width': canvas.width-(scaled_width+350), 'height': (scaled_height/2)};
-        let max_width = textbox.width - 150;
+        let textbox = {'x': (canvas.width/2)+50, 'y': (canvas.height/2)+100, 'width': canvas.width - (canvas.width/2)-50, 'height': (canvas.height/2)-100};
+        let max_width = textbox.width - 50;
         context.fillRect(textbox.x, textbox.y, textbox.width, textbox.height);
 
 
         // draw message
-        context.font = "100px impact";
-        let textWidth = context.measureText(this.text).width;
-
-
+        console.log('draw message '+this.text);
+        context.font = "50px helvetica";
         context.textAlign = 'left';
-        context.fillStyle = this.color;
+        context.fillStyle = '#000000';
         let lines = getLines(context,this.text,max_width);
-        console.log(lines);
-        let lines_y = textbox.y+150;
-        let line_height = 150;
+        let lines_y = textbox.y+100;
+        let line_height = 50;
         lines.forEach(line => {
           console.log(line);
-          context.fillText(line, scaled_width+250, lines_y, max_width);
+          context.fillText(line, scaled_width+150, lines_y, max_width);
           lines_y += line_height;
         });
-      } else {
-        //if not portrait then landscape image
 
-      }
 
       img.src = canvas.toDataURL();
     }
